e5481b0f57cb26271abf94961a3e5cfc8b6a3031
ifrs <ifrs@192.168.56.1>
2017-04-26 21:08:29 -0300

    foooi

---
+++ trabalhoRoger/src/trabalhoRoger/Conta.java
@@ -0,0 +1,46 @@
+package trabalhoRoger;
+
+import java.util.Scanner;
+
+
+
+
+public class Conta {
+	public String conta;
+	private double saldo = 0;
+	
+	public boolean deposita(double valor){
+		if (valor > 0){
+			saldo+=valor;
+			return true;
+		}
+		else{
+			System.out.println("Valor inválido!");
+			return false;
+		}
+	}
+	
+	public double getSaldo(){
+		return saldo;
+	}
+	
+	public boolean saca(double valor1){
+		if(valor1 <= this.saldo){
+			if(valor1 >= 0){
+				saldo -= valor1;
+				return true;
+			}else{
+				System.out.println("valor invalido");
+			}
+		}else{
+			System.out.println("saldo insuficiente");
+		}
+		return false;
+	}
+	
+	public double lerDouble(String mensagem){
+		System.out.println(mensagem);
+		Scanner in = new Scanner(System.in);
+		return in.nextDouble();
+	}
+}
---
+++ trabalhoRoger/src/trabalhoRoger/ExConta.java
@@ -0,0 +1,23 @@
+package trabalhoRoger;
+
+public class ExConta {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Conta c1 = new Conta();
+		
+		 double valor1 = c1.lerDouble("Informe a quantidade a depositar: "); 
+		 c1.deposita(valor1);
+		 
+		 System.out.println(c1.getSaldo());
+		 
+		 double valor2 = c1.lerDouble("Informe a quantidade a sacar: ");
+		 c1.saca(valor2);
+		 
+		 System.out.println ("Saldo: ");
+		 System.out.println(c1.getSaldo());
+	}
+
+}
