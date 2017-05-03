acbf22cbb0bf8bee6d2aac0a1375f42c60ea5b16
ifrs <ifrs@oso-79685>
2017-05-03 20:59:26 -0300

    5555666

--- trabalhoRoger/src/trabalhoRoger/Conta.java
+++ trabalhoRoger/src/trabalhoRoger/Conta.java
@@ -33,7 +33,7 @@ public class Conta {
 				System.out.println("valor n valido");
 			}
 		}else{
-			System.out.println("saldo n ssteste");
+			System.out.println("saldo n sst58258este");
 		}
 		return false;
 	}
