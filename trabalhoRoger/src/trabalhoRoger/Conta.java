912bb67c4d8b7dc53db48f4a796662cb92b6b663
ifrs <ifrs@192.168.56.1>
2017-05-03 22:47:32 -0300

    comitando pra testar

--- trabalhoRoger/src/trabalhoRoger/Conta.java
+++ trabalhoRoger/src/trabalhoRoger/Conta.java
@@ -33,7 +33,7 @@ public class Conta {
 				System.out.println("valor n valido");
 			}
 		}else{
-			System.out.println("saldo n insuficiente");
+			System.out.println("saldo n insficiente");
 		}
 		return false;
 	}
